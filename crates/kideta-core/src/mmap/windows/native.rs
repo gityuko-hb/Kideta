c124f1370c57d75fd53b1e6fe14dee532d63552b
Yumeio <yumeio0308@gmail.com>
2026-05-29 05:02:08 +0700

    ✨ feat(mmap): add flush_range, advise stubs, lock/unlock stubs, remap for Windows

--- crates/kideta-core/src/mmap/windows/native.rs
+++ crates/kideta-core/src/mmap/windows/native.rs
@@ -65,9 +65,22 @@ const MEM_COMMIT: u32 = 0x00001000;
 const MEM_RESERVE: u32 = 0x00002000;
 const MEM_RELEASE: u32 = 0x00008000;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct MadvFlags(u32);
+
+impl MadvFlags {
+    pub const NORMAL: MadvFlags = MadvFlags(0);
+    pub const SEQUENTIAL: MadvFlags = MadvFlags(0);
+    pub const RANDOM: MadvFlags = MadvFlags(0);
+    pub const WILLNEED: MadvFlags = MadvFlags(0);
+    pub const DONTNEED: MadvFlags = MadvFlags(0);
+    pub const POPULATE_READ: MadvFlags = MadvFlags(0);
+}
+
 pub struct Mmap {
     ptr: NonNull<c_void>,
     len: usize,
+    #[allow(dead_code)]
     mapping_handle: std::os::windows::io::RawHandle,
 }
 
@@ -162,6 +175,71 @@ impl Mmap {
             }
         }
     }
+
+    pub fn flush_range(&self, _offset: usize, _len: usize) -> MmapResult<()> {
+        self.flush()
+    }
+
+    pub fn flush_async(&self) -> MmapResult<()> {
+        self.flush()
+    }
+
+    pub fn flush_range_async(&self, _offset: usize, _len: usize) -> MmapResult<()> {
+        self.flush()
+    }
+
+    pub fn advise(&self, _advice: MadvFlags) -> MmapResult<()> {
+        Ok(())
+    }
+
+    #[inline]
+    pub fn advise_sequential(&self) -> MmapResult<()> {
+        Ok(())
+    }
+
+    #[inline]
+    pub fn advise_random(&self) -> MmapResult<()> {
+        Ok(())
+    }
+
+    #[inline]
+    pub fn advise_willneed(&self) -> MmapResult<()> {
+        Ok(())
+    }
+
+    #[inline]
+    pub fn advise_dontneed(&self) -> MmapResult<()> {
+        Ok(())
+    }
+
+    #[inline]
+    pub fn advise_populate_read(&self) -> MmapResult<()> {
+        Ok(())
+    }
+
+    pub fn lock(&self) -> MmapResult<()> {
+        Ok(())
+    }
+
+    pub fn unlock(&self) -> MmapResult<()> {
+        Ok(())
+    }
+
+    pub fn remap(&mut self, _new_len: usize) -> MmapResult<()> {
+        Err(MmapError::Map {
+            reason: "remap not supported on Windows; recreate mapping".to_string(),
+            code: None,
+        })
+    }
+}
+
+impl std::fmt::Debug for Mmap {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("Mmap")
+            .field("addr", &self.ptr)
+            .field("len", &self.len)
+            .finish()
+    }
 }
 
 impl Drop for Mmap {
@@ -178,9 +256,7 @@ impl Drop for Mmap {
 }
 
 pub struct MmapMut {
-    ptr: NonNull<c_void>,
-    len: usize,
-    mapping_handle: std::os::windows::io::RawHandle,
+    pub(crate) inner: Mmap,
 }
 
 unsafe impl Send for MmapMut {}
@@ -214,11 +290,13 @@ impl MmapMut {
             return Err(map_error("MapViewOfFile failed"));
         }
 
-        Ok(Self {
+        let inner = Mmap {
             ptr: NonNull::new(ptr).unwrap(),
             len,
             mapping_handle: mapping,
-        })
+        };
+
+        Ok(Self { inner })
     }
 
     pub unsafe fn map_anonymous_mut(len: usize) -> MmapResult<Self> {
@@ -226,67 +304,122 @@ impl MmapMut {
             return Err(MmapError::ZeroSize);
         }
         let mmap = unsafe { Mmap::map_anonymous(len) }?;
-        let ptr = mmap.ptr;
-        let mapping_handle = mmap.mapping_handle;
-        std::mem::forget(mmap);
-        Ok(Self {
-            ptr,
-            len,
-            mapping_handle,
-        })
+        Ok(Self { inner: mmap })
     }
 
     #[inline]
     pub fn len(&self) -> usize {
-        self.len
+        self.inner.len()
     }
 
     #[inline]
     pub fn is_empty(&self) -> bool {
-        self.len == 0
+        self.inner.is_empty()
     }
 
     #[inline]
     pub fn as_ptr(&self) -> *const c_void {
-        self.ptr.as_ptr()
+        self.inner.as_ptr()
     }
 
     #[inline]
     pub fn as_mut_ptr(&mut self) -> *mut c_void {
-        self.ptr.as_ptr()
+        self.inner.as_mut_ptr()
     }
 
     #[inline]
     pub unsafe fn as_slice(&self) -> &[u8] {
-        unsafe { std::slice::from_raw_parts(self.ptr.as_ptr() as *const u8, self.len) }
+        self.inner.as_slice()
     }
 
     #[inline]
     pub unsafe fn as_mut_slice(&mut self) -> &mut [u8] {
-        unsafe { std::slice::from_raw_parts_mut(self.ptr.as_ptr() as *mut u8, self.len) }
+        self.inner.as_mut_slice()
     }
 
     pub fn flush(&self) -> MmapResult<()> {
-        unsafe {
-            if bindings::FlushViewOfFile(self.ptr.as_ptr(), self.len) != 0 {
-                Ok(())
-            } else {
-                Err(map_error("FlushViewOfFile failed"))
-            }
-        }
+        self.inner.flush()
+    }
+
+    pub fn flush_sync(&self) -> MmapResult<()> {
+        self.inner.flush()
+    }
+
+    pub fn flush_async(&self) -> MmapResult<()> {
+        self.inner.flush_async()
+    }
+
+    pub fn flush_range(&self, offset: usize, len: usize) -> MmapResult<()> {
+        self.inner.flush_range(offset, len)
+    }
+
+    pub fn advise(&self, advice: MadvFlags) -> MmapResult<()> {
+        self.inner.advise(advice)
+    }
+
+    #[inline]
+    pub fn advise_sequential(&self) -> MmapResult<()> {
+        self.inner.advise_sequential()
+    }
+
+    #[inline]
+    pub fn advise_random(&self) -> MmapResult<()> {
+        self.inner.advise_random()
+    }
+
+    #[inline]
+    pub fn advise_willneed(&self) -> MmapResult<()> {
+        self.inner.advise_willneed()
+    }
+
+    #[inline]
+    pub fn advise_dontneed(&self) -> MmapResult<()> {
+        self.inner.advise_dontneed()
+    }
+
+    #[inline]
+    pub fn advise_populate_read(&self) -> MmapResult<()> {
+        self.inner.advise_populate_read()
+    }
+
+    pub fn lock(&self) -> MmapResult<()> {
+        self.inner.lock()
+    }
+
+    pub fn unlock(&self) -> MmapResult<()> {
+        self.inner.unlock()
+    }
+
+    pub fn remap(&mut self, new_len: usize) -> MmapResult<()> {
+        self.inner.remap(new_len)
+    }
+}
+
+impl std::fmt::Debug for MmapMut {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("MmapMut")
+            .field("addr", &self.inner.ptr)
+            .field("len", &self.inner.len)
+            .finish()
     }
 }
 
 impl Drop for MmapMut {
     fn drop(&mut self) {
-        unsafe {
-            if self.mapping_handle.is_null() {
-                bindings::VirtualFree(self.ptr.as_ptr(), 0, MEM_RELEASE);
-            } else {
-                bindings::UnmapViewOfFile(self.ptr.as_ptr());
-                bindings::CloseHandle(self.mapping_handle);
-            }
-        }
+        // Mmap's Drop handles the cleanup
+    }
+}
+
+impl std::ops::Deref for MmapMut {
+    type Target = Mmap;
+    fn deref(&self) -> &Mmap {
+        &self.inner
+    }
+}
+
+impl std::ops::DerefMut for MmapMut {
+    fn deref_mut(&mut self) -> &mut Mmap {
+        &mut self.inner
     }
 }
 
